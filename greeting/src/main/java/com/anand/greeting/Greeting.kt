5be3e75196d2e77a6380a6bc7d171eb07fc49e53
Amrita Rana <amuraamurana2201@gmail.com>
2023-08-03 19:16:18 +0530

    Initial commit

---
+++ greeting/src/main/java/com/anand/greeting/Greeting.kt
@@ -0,0 +1,14 @@
+package com.anand.greeting
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
